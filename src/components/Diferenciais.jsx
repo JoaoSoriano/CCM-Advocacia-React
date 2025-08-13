d28cb4a17d18ac2b39a6b5d00047126eb0d76f4c
João Soriano <joaorafael2502@gmail.com>
2025-08-13 01:36:52 -0300

    arrow

--- src/components/Diferenciais.jsx
+++ src/components/Diferenciais.jsx
@@ -50,7 +50,7 @@ const Diferenciais = () => {
                   <Link to={item.link} className="card-link">
                     <img src={item.img} alt={item.alt} className="card-image" />
                     <h2 className="card-title">{item.title}</h2>
-                    <button className='card-button material-symbols-outlined'>read_more</button>
+                    <button className='card-button material-symbols-outlined'>⮎</button>
                   </Link>
                 </SwiperSlide>
               ))}
